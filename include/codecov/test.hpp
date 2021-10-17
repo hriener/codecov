fbbdb4ce7871979c097fa623c61d32b39d9f74ba
Heinz Riener <hriener@london.fritz.box>
2021-10-17 13:19:47 +0200

    initial.

---
+++ include/codecov/test.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+namespace amazing
+{
+
+inline std::ostream& awesome( std::ostream& os ) {
+  return os << "hello from awesome\n";
+}
+
+} // namespace amazing
+
---
+++ test/run_tests.cpp
@@ -0,0 +1,7 @@
+#include <codecov/test.hpp>
+
+int main()
+{
+  amazing::awesome( std::cout );
+  return EXIT_SUCCESS;
+}
