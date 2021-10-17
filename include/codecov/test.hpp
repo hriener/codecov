32b1490040777516de96f518adc6801fba68907f
Heinz Riener <hriener@london.fritz.box>
2021-10-17 13:47:32 +0200

    another try.

--- include/codecov/test.hpp
+++ include/codecov/test.hpp
@@ -7,9 +7,15 @@ inline std::ostream& awesome( std::ostream& os ) {
   return os << "hello from awesome\n";
 }
 
-inline std::ostream& more_awesome_but_not_covered( std::ostream& os ) {
-  return os << "hi, hello, and how are you? :)) from more_awesome\n";
+inline std::ostream& more_awesome_but_not_covered( std::ostream& os, bool flag ) {
+  if ( flag )
+  {
+    return os << "hi, #1 from more_awesome\n";
+  }
+  else
+  {
+    return os << "hi, #2 from more_awesome\n";
+  }
 }
-  
 } // namespace amazing
 
