fbef496ab1b8f5031df342c87825665af98fbae6
Heinz Riener <hriener@london.fritz.box>
2021-10-17 13:42:54 +0200

    uncovered code.

--- include/codecov/test.hpp
+++ include/codecov/test.hpp
@@ -7,5 +7,9 @@ inline std::ostream& awesome( std::ostream& os ) {
   return os << "hello from awesome\n";
 }
 
+inline std::ostream& more_awesome_but_not_covered( std::ostream& os ) {
+  return os << "hi, hello, and how are you? :)) from more_awesome\n";
+}
+  
 } // namespace amazing
 
